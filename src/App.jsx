2fdd06c67f5e01e14cea68bb936cad947f2e17b3
ArtemGoiT <150847319+ArtemGoiT@users.noreply.github.com>
2024-06-16 09:29:44 +0200

    fix

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </>
+  );
+};
+
+export default App;
